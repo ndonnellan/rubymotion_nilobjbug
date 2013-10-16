b8f05964f7caf1c6afcfc8370b00892aefc2ad23
Nathan Donnellan <ndonnellan@gmail.com>
2013-10-16 17:23:37 -0500

    Add annotations

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,22 +3,31 @@ Queue2 = Dispatch::Queue.concurrent('com.testing.queue2')
 
 def fun
   Queue1.async do
-    ptr = Pointer.new(:object)
-    return
+    # Async block must create a pointer
+    ptr = Pointer.new(:object) 
+
+    # It also must return (even though this does nothing)
+    return                     
   end
 end
 
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
 
-    fun
+    fun # Call function that calls async and returns
     
     Queue2.async do
-      obj = ClassWithPointer.new
+      # Try to create obj that has a @ptr attribute
+      obj = ClassWithPointer.new    
+
+      # 'obj' will now be nil, and calling any methods on it
+      # other than 'nil?' will result in an uncaught exception
       obj.speak
+
+      # Interestingly, trying to 'raise' an exception after checking for nil will also cause a separate uncaught exception to be thrown
     end
 
-    puts "Success!"
+    puts "Success!" # This line never gets called
 
     true
   end
